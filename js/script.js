5284499559ff704bd30a49326605ebb981c93ef9
Devmax-0 <muhammadhamzashahid31@gmail.com>
2023-12-28 00:41:30 +0500

    Nothing

--- js/script.js
+++ js/script.js
@@ -1,32 +1,29 @@
 const navlinks = document.querySelectorAll(".link");
 
-navlinks.forEach(navlinks => {
-    navlinks.addEventListener("click", () => {
-        document.querySelector(".active").classList.remove("active")
-        navlinks.classList.add("active")
-    })
-})
-
-
+navlinks.forEach((navlinks) => {
+  navlinks.addEventListener("click", () => {
+    document.querySelector(".active").classList.remove("active");
+    navlinks.classList.add("active");
+  });
+});
 
 const menuBtn = document.querySelector(".menu-btn");
 
 menuBtn.addEventListener("click", () => {
-    document.querySelector(".sidenav").style.marginLeft = "0px";
-    document.querySelector(".sidenav").style.transition = "all 700ms";
-})
+  document.querySelector(".sidenav").style.marginLeft = "0px";
+  document.querySelector(".sidenav").style.transition = "all 700ms";
+});
 
 const closeBtn = document.querySelector(".close-btn");
 
 closeBtn.addEventListener("click", () => {
-    document.querySelector(".sidenav").style.marginLeft = "-270px";
-    document.querySelector(".sidenav").style.transition = "all 700ms";
-})
-
+  document.querySelector(".sidenav").style.marginLeft = "-270px";
+  document.querySelector(".sidenav").style.transition = "all 700ms";
+});
 
 var typed = new Typed(".auto-type-words", {
-    strings: ["Web Dev.", "Frontend Dev.", "UI/UX Dev."],
-    typeSpeed: 120,
-    backSpeed: 120,
-    loop: true
-})
+  strings: ["Web Dev.", "Frontend Dev.", "UI/UX Dev."],
+  typeSpeed: 120,
+  backSpeed: 120,
+  loop: true,
+});
