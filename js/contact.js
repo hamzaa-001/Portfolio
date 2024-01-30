7b2b347785020f4e70290d518d7ea40f328af3b3
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-01-30 19:37:27 +0500

    fixed repository changes

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const navlinks = document.querySelectorAll(".link");
 
 navlinks.forEach((navlinks) => {
@@ -38,44 +37,11 @@ function sendMail() {
       successMsg.style.display = "block";
     });
 }
-=======
-const navlinks = document.querySelectorAll(".link");
 
-navlinks.forEach((navlinks) => {
-  navlinks.addEventListener("click", () => {
-    document.querySelector(".active").classList.remove("active");
-    navlinks.classList.add("active");
-  });
+let backBtn = document.querySelector(".backBtn");
+backBtn.addEventListener("click", function () {
+  let form = document.querySelector(".input-form");
+  let successMsg = document.querySelector(".success-msg-wrapper");
+  successMsg.style.display = "none";
+  form.style.display = "block";
 });
-
-const menuBtn = document.querySelector(".menu-btn");
-
-menuBtn.addEventListener("click", () => {
-  document.querySelector(".sidenav").style.marginLeft = "0px";
-  document.querySelector(".sidenav").style.transition = "all 700ms";
-});
-
-const closeBtn = document.querySelector(".close-btn");
-
-closeBtn.addEventListener("click", () => {
-  document.querySelector(".sidenav").style.marginLeft = "-270px";
-  document.querySelector(".sidenav").style.transition = "all 700ms";
-});
-
-function sendMail() {
-  var params = {
-    form_name: document.getElementById("name").value,
-    email_id: document.getElementById("email").value,
-    message: document.getElementById("message").value,
-  };
-  emailjs
-    .send("service_yzoe5cv", "template_w63w1rp", params)
-    .then(function (res) {
-      let form = document.querySelector(".input-form");
-      let successMsg = document.querySelector(".success-msg-wrapper");
-
-      form.style.display = "none";
-      successMsg.style.display = "block";
-    });
-}
->>>>>>> 01695339a3c337f85a1069eb7edd574ad960c6ee
